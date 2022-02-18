6d998b17612cb631ed5562473a8ffc731626acf6
Benoit-thb <84345832+Benoit-thb@users.noreply.github.com>
2022-02-19 00:01:27 +0100

    Update blague.js

--- blague.js
+++ blague.js
@@ -6,11 +6,21 @@ document.addEventListener('click', function(e){
 
 	var elem = document.getElementById('btn');
 	var audio = document.getElementById('oh');
+	var zem = document.getElementById('bloc');
 
 	if(e.target.id == "btn")
 	{
 		elem.style.width = 300 + "px";
 		audio.play();
+		elem.style.display = "none";
+		zem.style.animationPlayState = "running";
+	}
+	if(e.target.id == "tap")
+	{
+		elem.style.width = 300 + "px";
+		audio.play();
+		elem.style.display = "none";
+		zem.style.animationPlayState = "running";
 	}
 	console.log(e.target.id);
 
